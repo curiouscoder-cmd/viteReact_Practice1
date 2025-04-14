78a0ae20d8c3aedb7b5550a737d8ac8913eea235
Nitya Jain <nityaprofessional6402@gmail.com>
2025-04-14 14:15:37 +0530

    added more buttons and adde estimated time, word count

--- src/Components/TextForm.jsx
+++ src/Components/TextForm.jsx
@@ -7,19 +7,43 @@ export default function TextForm(props) {
         let newTEXT = text.toUpperCase();
         setText(text.toUpperCase());
     }
+
+    const handleLoClick = () => {
+        console.log("Uppercase was clicked" + text);
+        let newTEXT = text.toLowerCase();
+        setText(text.toLowerCase());
+    }
+    const handleClearClick = () => {
+        console.log("text cleaned" );
+        let newTEXT = '';
+        setText('');
+    }
+
     const handleOnChange = (event) => {
         console.log("On change");
         setText(event.target.value);
     }
-const [text, setText] = useState("Enter text here");
+const [text, setText] = useState("");
   return (
-    <div>
+    <>
+    <div className = 'container'>
 <h1>{props.heading} </h1>
 <div className="mb-3">
 <label htmlFor="myBox" className="form-label">Example textarea</label>
 <textarea className="form-control" value ={text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+<button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to Uppercase</button>
+<button className='btn btn-primary mx-1' onClick={handleLoClick}>Convert to Lowercase</button>
+<button className='btn btn-primary mx-1' onClick={handleClearClick}>Clear Text</button>
 </div>
+<div className="container my-3">
+    <h2>Your Text summary</h2>
+    <p> {text.split(' ').length} and {text.length} characters</p>
+    <p>{0.008 *text.split(' ').length} Minutes read</p>
+    <h2>Preview</h2>
+    <p>{text}</p>
+    
+</div>
+</>
   )
 }
